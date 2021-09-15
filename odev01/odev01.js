0253bbdb2ee86fe57a1e0155da067b9a30e0b24c
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-15 16:42:44 +0300

    log yazısı eklendi

--- odev01/odev01.js
+++ odev01/odev01.js
@@ -2,7 +2,7 @@ let r = process.argv.slice(2)[0];
 
 function daireAlani(r) {
   let sonuc = 3.14 * Number(r * r);
-  console.log(sonuc);
+  console.log(`Yarıçapı ${r} olan dairenin alanı: ${sonuc}`);
 }
 
 daireAlani(r);
