b4b7084453e2e2b96e99bb9fd8cc5a5ad1853ef9
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-17 10:00:27 +0300

    odev03

---
+++ odev03/index.js
@@ -0,0 +1,4 @@
+const { circleArea, circleCircumference } = require(`./odev03.js`);
+
+circleArea(5);
+circleCircumference(5);
---
+++ odev03/odev03.js
@@ -0,0 +1,12 @@
+function circleArea(r) {
+  console.log(3.14 * (r * r));
+}
+
+function circleCircumference(r) {
+  console.log(2 * 3.14 * r);
+}
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
