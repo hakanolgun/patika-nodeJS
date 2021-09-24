0e5aa71856f956d1d0e5359f101925d165891fbc
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-24 20:28:02 +0300

    PCAT fotoğraf yükleme ve sayfada gösterme tamam

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const Photo = require('./models/Photo');
-
+const fs = require('fs');
 const app = express();
 
 //connect DB
@@ -19,10 +20,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // Routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -44,9 +46,22 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  console.log(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
