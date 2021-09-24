c65ca20ae1a4e5e55d7f988ce243616d832c8ae6
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-24 18:48:06 +0300

    clean blog veri tabanına post ekleme tamam. pcat hoca ile beraber tamam

--- CLEAN_BLOG/app.js
+++ CLEAN_BLOG/app.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const path = require("path");
+const Post = require("./models/Post");
+
 const app = express();
+
+//connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //template engine
 app.set("view engine", "ejs");
 
 // Mıddleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add_post", (req, res) => {
   res.render("add_post");
+});
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  console.log(req.body);
+  res.redirect('/');
 });
 
 const port = 3377;
--- PCAT/app.js
+++ PCAT/app.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const path = require('path');
+const Photo = require('./models/Photo');
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //template engine
 app.set('view engine', 'ejs');
 
 // Mıddleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add', (req, res) => {
   res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  console.log(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
