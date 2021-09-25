434a69ec5cabc8a78266378f9e36fdff0ef0485a
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-25 10:59:31 +0300

    pcat fotoğraf silme tamam

--- PCAT/app.js
+++ PCAT/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require('path');
 const Photo = require('./models/Photo');
 const fs = require('fs');
@@ -21,6 +22,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // Routes
 app.get('/', async (req, res) => {
@@ -69,6 +75,23 @@ app.get('/photos/edit/:id', async (req, res) => {
   res.render('edit', {
     photo,
   });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
 });
 
 const port = 3000;
